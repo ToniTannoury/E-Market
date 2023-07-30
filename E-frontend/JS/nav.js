38660da1574206afe180c9ab4bc46714687f7c30
ToniTannoury <tatannoury01@gmail.com>
2023-07-31 02:34:59 +0300

    Implemented nav linking

---
+++ E-frontend/JS/nav.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const shoppingCartLink = document.querySelector(".shoppingCart-link");
+  const favoritesLink = document.querySelector(".favorites-link");
+  const yourProductsLink = document.querySelector(".yourProducts-link");
+  const marketLink = document.querySelector(".market");
+  const profileLink = document.querySelector(".profile-link");
+
+  shoppingCartLink.addEventListener("click", () => {
+    window.location.href = "cart.html";
+  });
+
+  favoritesLink.addEventListener("click", () => {
+    window.location.href = "favorites.html";
+  });
+
+  yourProductsLink.addEventListener("click", () => {
+    window.location.href = "yourProducts.html";
+  });
+
+  marketLink.addEventListener("click", () => {
+    window.location.href = "allProducts.html";
+  });
+
+  profileLink.addEventListener("click", () => {
+    window.location.href = "profile.html";
+  });
+});
